938522a8e386727cb7a81d4ac3d220f297608fbd
39312657 <tacjka34@gmail.com>
2020-05-09 16:19:57 +0300

    Defined routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get '/posts', to: 'posts#index'
+  get '/posts/:id', to: 'posts#show'
+  get '/posts/new', to: 'posts#new'
+  post '/posts', to: 'posts#create'
+  get '/posts/:id/edit', to: 'posts#edit'
+  patch '/posts/:id', to: 'posts#update'
+  delete '/posts/:id', to: 'posts#delete'
 end
