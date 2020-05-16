73b73695cc554f1f2d03692bddb4cab60fee4793
39312657 <tacjka34@gmail.com>
2020-05-16 14:04:46 +0300

    commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get '/posts', to: 'posts#index'
-  get '/posts/:id', to: 'posts#show'
+  get '/posts', to: 'posts#index', as: :main
   get '/posts/new', to: 'posts#new'
+  get '/posts/:id', to: 'posts#show', as: :post
   post '/posts', to: 'posts#create'
-  get '/posts/:id/edit', to: 'posts#edit'
+  get '/posts/:id/edit', to: 'posts#edit', as: :edit
   patch '/posts/:id', to: 'posts#update'
   delete '/posts/:id', to: 'posts#delete'
 end
